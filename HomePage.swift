2f4246cf06340d5a41f53b2f3505c6f89f508b03
Nishaykumar <Nishaykumar24@gmail.com>
2023-05-03 02:02:41 +0530

    BUG FIXED. Added "static var sheetHasBeenPresented = false". This way the user can only see the .sheet once when transitioning from onBoardingScreen to Home

--- HomePage.swift
+++ HomePage.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 class HomePage: ObservableObject{
     @Published var selectedTab = "home"
-    @Published var isShowingSheet = false 
+    @Published var isShowingSheet = false
 }
 
 
 struct Home: View{
     @StateObject var homePage = HomePage() // add an instance of HomePage
     
+    static var sheetHasBeenPresented = false
     var body: some View {
         ZStack {
             VStack {
@@ -29,14 +30,16 @@ struct Home: View{
             
         }
         .onAppear {
-            homePage.isShowingSheet = true
+            print(Home.sheetHasBeenPresented)
+            if !Home.sheetHasBeenPresented {
+                homePage.isShowingSheet = true
+                Home.sheetHasBeenPresented = true
+            }
+            print(Home.sheetHasBeenPresented)
         }
         .sheet(isPresented: $homePage.isShowingSheet) {
             IntroSheetView()
-
         }
-        
-        
         
     }
 }
